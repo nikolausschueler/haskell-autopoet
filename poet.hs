189b043591687a0d6848cbd8e1df64892dcb22b6
Nik <nik@drnik.org>
2019-06-19 12:33:02 +0200

    We can now handle matches at the end of the list
    In this case, a sentinel value is used.

--- poet.hs
+++ poet.hs
@@ -1,7 +1,32 @@
 import Data.List
+import Data.Maybe
 
-successors :: Eq a => [a] -> [a] -> [a]
-successors s [] = []
-successors s (x:xs)
-  | isPrefixOf s (x:xs) = head (drop (length s) (x:xs)) : successors s xs
-  | otherwise = successors s xs
+successor :: Eq a => [a] -> [a] -> Maybe a
+successor s (x:xs)
+  | length s == length (x:xs) = Nothing
+  | otherwise = Just (head (drop (length s) (x:xs)))
+
+{-
+  This function needs an additional "sentinel" parameter. The sentinel is used
+  when the Maybe has the value "Nothing", which indicates that the match was at
+  the end of the list and we don't have a real successor. Because Haskell is
+  strongly typed, we cannot use something generic like Java's "null" to indicate
+  that the successor matched right at the end of the list.
+
+  So we have to use a sentinel that fits the type of the list elements. For
+  strings (aka lists of characters), the ETX (End of TeXt) seems a good choice.
+  For other lists (like a list of musical notes, for example), we would have to
+  choose a different sentinel.
+
+  The function can be used like this:
+
+  *Main> successors "the" "theathebthe" '\ETX'
+  "ab\ETX"
+  *Main> successors "the" "theathebtheb" '\ETX'
+  "abb"
+-}
+successors :: Eq a => [a] -> [a] -> a -> [a]
+successors s [] sentinel = []
+successors s (x:xs) sentinel
+  | isPrefixOf s (x:xs) = fromMaybe sentinel (successor s (x:xs)) : successors s xs sentinel
+  | otherwise = successors s xs sentinel
