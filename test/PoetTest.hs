4fa47aa26ee8e7ac57eeae69680fa12a60b8329d
Nik <nik@drnik.org>
2024-01-03 17:23:02 +0100

    Run more than one test

--- test/PoetTest.hs
+++ test/PoetTest.hs
@@ -4,7 +4,12 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Poet
 
-main = defaultMain $
-  testCase "Simple test" $ do
+main = defaultMain tests
+tests = testGroup "Tests"
+  [ testCase "Simple test" $ do
     assertEqual "Successor of substring of same lenght as text"
       ['\ETX'] (successors "abc" "abc" '\ETX')
+  , testCase "Even simpler test" $ do
+    assertEqual "Successor of substring of same lenght as text"
+      ['\ETX'] (successors "ab" "ab" '\ETX')
+  ]
